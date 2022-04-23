68ff9e4a9e0e745a39b999324b21477a02471b15
Mathieu <mathieu.melek@edu.devinci.fr>
2022-04-23 18:35:50 +0200

    Begnning

---
+++ Assets/DrumExercice/Scripts/BallSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform spawnPosition;
+    public float delayBeforeStart;
+    public float delayBetweenSpawn;
+    public bool shouldDestroyBallOnHit;
+    public float delayBeforeBallDestruction;
+
+    void Start()
+    {
+        StartCoroutine(SpawnBall());
+    }
+
+    IEnumerator SpawnBall()
+    {
+        yield return new WaitForSeconds(delayBeforeStart);
+
+        while (true)
+        {
+            GameObject tmpGO = Instantiate(prefab, spawnPosition.position, Quaternion.identity);
+            
+            if (shouldDestroyBallOnHit)
+            {
+                tmpGO.GetComponent<BallBehavior>().shouldBeDestroyedOnHit = true;
+            }
+            else
+            {
+                Destroy(tmpGO, delayBeforeBallDestruction);
+            }
+
+            yield return new WaitForSeconds(delayBetweenSpawn);
+        }
+    }
+}
---
+++ Assets/DrumExercice/Scripts/LaunchSound.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioHelm;
+
+public class LaunchSound : MonoBehaviour
+{
+    //[Range(60, 71)]
+    //public int soundToPlay;
+
+    public DrumElements drumElement = DrumElements.Grosse_Caisse;
+
+    // 60 : Grosse caisse
+    // 61 : Caisse claire
+    // 62 : Tom alto
+    // 63 : Tom medium
+    // 64 : Tom basse
+    // 65 : CP
+    // 66 : Rimshot
+    // 67 : Cow bell
+    // 68 : Charleston fermee
+    // 69 : Charleston ouverte
+    // 70 : Cymbale Ride
+    // 71 : Cymbale Crash
+    public enum DrumElements
+    {
+        Grosse_Caisse = 60,
+        Caisse_Claire,
+        Tom_Alto,
+        Tom_Medium,
+        Tom_Basse,
+        Clap,
+        Rimshot,
+        Cow_Bell,
+        Charleston_Fermee,
+        Charleston_Ouverte,
+        Cymbale_Ride,
+        Cymbale_Crash
+    }
+
+    private Sampler drumSampler;
+
+    private void Awake()
+    {
+        drumSampler = GameObject.Find("Drum Sampler").GetComponent<Sampler>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(drumSampler)
+        {
+            //drumSampler.NoteOn(soundToPlay, 1.0f);
+            drumSampler.NoteOn((int)drumElement, 1.0f);
+            if (collision.gameObject.GetComponent<BallBehavior>().shouldBeDestroyedOnHit)
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
